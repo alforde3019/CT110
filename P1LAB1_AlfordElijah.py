9714c9b018be5d3fbdb7373002c9a3d59e01e49c
alforde3019 <166688166+alforde3019@users.noreply.github.com>
2024-04-11 02:11:33 -0400

    Add files via upload

---
+++ P1LAB1_AlfordElijah.py
@@ -0,0 +1,21 @@
+# Elijah Alford
+# April 11,2024
+# P1LAB1
+# Create a program that allows the user to enter their first name and last name.
+
+firstname = input("Enter your first name:")
+
+lastname = input("Enter your last name:")
+
+print("Hello,",firstname,lastname, end="! Welcome to CTI-110")
+
+
+
+
+
+
+
+
+
+
+

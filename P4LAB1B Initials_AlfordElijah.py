dc4a8d9f5f07f3b6ae2cb2a06d1dd174151ea3f7
alforde3019 <166688166+alforde3019@users.noreply.github.com>
2024-05-09 03:30:36 -0400

    Add files via upload

---
+++ P4LAB1B Initials_AlfordElijah.py
@@ -0,0 +1,44 @@
+import turtle          
+win = turtle.Screen()  
+e = turtle.Turtle()
+
+
+e.pensize(4)           
+e.pencolor("blue")     
+e.shape("turtle")
+
+e.forward(100)
+e.backward(100)
+
+e.right(90)
+e.forward(100)
+
+e.left(90)
+e.forward(100)
+e.backward(100)
+e.right(90)
+
+e.forward(100)
+e.left(90)
+e.forward(100)
+
+e.penup()
+e.forward(100)
+e.pendown()
+
+e.forward(130)
+e.right(45)
+e.forward(50)
+e.backward(150)
+
+e.right(90)
+e.forward(150)
+
+
+
+
+
+
+
+
+
---
+++ P4LAB1a Shapes_AlfordElijah.py
@@ -0,0 +1,39 @@
+import turtle
+win = turtle.Screen()
+alex = turtle.Turtle()
+
+for x in range(4):
+    alex.forward(100)
+    alex.left(90)
+
+alex.penup()
+
+alex.forward(200)
+
+alex.pendown()
+
+for x in range(6):
+    alex.forward(100)          
+    alex.left(120)            
+    alex.forward(100)
+    alex.left(120)
+    
+
+alex.penup()
+
+alex.forward(200)
+
+alex.pendown()
+
+win.mainloop()
+
+
+
+for x in range(4):
+            e.forward(130)
+            e.right(45)
+            e.forward(50)
+            e.backward(150)
+
+            e.right(90)
+            e.forward(150)
